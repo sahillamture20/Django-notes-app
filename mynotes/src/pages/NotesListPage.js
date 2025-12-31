cd37612ec262eebe6fd68e67d41e4c3746c50b5b
Sahil Lamture <76159336+sahillamture20@users.noreply.github.com>
2025-12-31 12:45:12 +0530

    Update NotesListPage.js

--- mynotes/src/pages/NotesListPage.js
+++ mynotes/src/pages/NotesListPage.js
@@ -21,7 +21,7 @@ const NotesListPage = () => {
         <div className='notes'>
             <div className="notes-header">
                 <h2 className="notes-title">
-                    &#9782; Notes
+                    &#9782; All Notes
                 </h2>
                 <p className="notes-count">{notes.length}</p>
             </div>
